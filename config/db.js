b02b7df410387a4de476ff798210538e21fc1a23
Allan Baptista <allan.marques@ymail.com>
2015-10-16 02:35:33 -0300

    Added prod db config

--- config/db.js
+++ config/db.js
@@ -4,10 +4,14 @@ const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 var jsonConfig = require('./database')[IS_PRODUCTION ? 'prod' : 'dev'];
 
-var config = Object.assign({}, jsonConfig, {
-    connectionLimit: 100,
-    debug: true
-});
+if (typeof jsonConfig === 'string') {
+    config = jsonConfig;
+} else {
+    var config = Object.assign({}, jsonConfig, {
+        connectionLimit: 100,
+        debug: true
+    });
+}
 
 if (IS_PRODUCTION) {
     config.debug = false;
